4013ae7873e3fc053a8ce9386d480dd7e8ecd71e
bitchdonotloveyou <64248540+bitchdontloveyou@users.noreply.github.com>
2020-11-10 16:42:10 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -6,16 +6,28 @@ api_hash = '31e44198df1635fa81c45fd8a23ba8e0'
 with TelegramClient('name', api_id, api_hash) as client :
     nixya = 5
     while (nixya < 55) :
-        privets = ['Привет', 'Здравствуйте', 'Добрый день', 'Добрый вечер', 'Доброе утро', 'Хай', 'Хелло', 'Халло', 'Доброго времени суток', 'Здравствуй', 'Здрасти']
-        nachalo = random.choice(privets) 
-        client.send_message('@RuAnon_bot', '/stop')
-        time.sleep(1)
-        client.send_message('@RuAnon_bot', '/start') 
-        time.sleep(3)
-        client.send_message('@RuAnon_bot', nachalo)
-        client.send_message('@RuAnon_bot', 'Работа интересует?')
-        client.send_message('@RuAnon_bot', 'Без привязки к месту и возврасту.') 
-        client.send_message('@RuAnon_bot', 'Подойдет даже школьникам.') 
-        client.send_message('@RuAnon_bot', 'Не криминал (не работа закладчиком и т.д.)')
-        client.send_message('@RuAnon_bot', 'Если интересует пиши сюда: @abbcddd . ')
-        time.sleep(10)
+        pytin = random.randint(0, 1)
+        if (pytin == 0) : 
+            privets = ['Привет', 'Здравствуйте', 'Добрый день', 'Добрый вечер', 'Доброе утро', 'Хай', 'Хелло', 'Халло', 'Доброго времени суток', 'Здравствуй', 'Здрасти']
+            nachalo = random.choice(privets) 
+            client.send_message('@RuAnon_bot', '/stop')
+            time.sleep(1)
+            client.send_message('@RuAnon_bot', '/start') 
+            time.sleep(3)
+            client.send_message('@RuAnon_bot', nachalo)
+            client.send_message('@RuAnon_bot', 'Работа интересует?')
+            client.send_message('@RuAnon_bot', 'Без привязки к месту и возврасту.') 
+            client.send_message('@RuAnon_bot', 'Подойдет даже школьникам.') 
+            client.send_message('@RuAnon_bot', 'Не криминал (не работа закладчиком и т.д.)')
+            client.send_message('@RuAnon_bot', 'Если интересует пиши сюда: @abbcddd . ')
+            time.sleep(10)
+        if (pytin == 1) :
+            privets = ['Привет', 'Здравствуйте', 'Добрый день', 'Добрый вечер', 'Доброе утро', 'Хай', 'Хелло', 'Халло', 'Доброго времени суток', 'Здравствуй', 'Здрасти']
+            nachalo = random.choice(privets) 
+            client.send_message('@RuAnon_bot', '/stop')
+            time.sleep(1)
+            client.send_message('@RuAnon_bot', '/start') 
+            time.sleep(3)
+            client.send_message('@RuAnon_bot', nachalo)
+            client.send_message('@RuAnon_bot', 'Работа интересует? \n Без привязки к месту и возврасту. \n Подойдет даже школьникам. \n Не криминал (не работа закладчиком и т.д.) \n Если интересует пиши сюда: @abbcddd . \n' )
+            
