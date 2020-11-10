e509123d807fb1235a2ae3b6093930eff5ab9b42
bitchdonotloveyou <64248540+bitchdontloveyou@users.noreply.github.com>
2020-11-10 12:47:02 +0300

    Update 8.py

--- 8.py
+++ 8.py
@@ -9,15 +9,15 @@ api_hash = '31e44198df1635fa81c45fd8a23ba8e0'
 with TelegramClient('name', api_id, api_hash) as client :
     nixya = 5
     while (nixya < 55) :
-        client.send_message('Анонимный чат Телеграма', '/stop')
+        client.send_message('@RuAnon_bot', '/stop')
         time.sleep(5)
-        client.send_message('Анонимный чат Телеграма', '/start') 
+        client.send_message('@RuAnon_bot', '/start') 
         time.sleep(10)
-        client.send_message('Анонимный чат Телеграма', 'Работа интересует?')
-        client.send_message('Анонимный чат Телеграма', 'Без привязки к месту и возврасту.') 
-        client.send_message('Анонимный чат Телеграма', 'Подойдет даже школьникам.') 
-        client.send_message('Анонимный чат Телеграма', 'Не криминал (не работа закладчиком и т.д.)')
-        client.send_message('Анонимный чат Телеграма', 'Если интересует пиши сюда: @abbcddd . ')
+        client.send_message('@RuAnon_bot', 'Работа интересует?')
+        client.send_message('@RuAnon_bot', 'Без привязки к месту и возврасту.') 
+        client.send_message('@RuAnon_bot', 'Подойдет даже школьникам.') 
+        client.send_message('@RuAnon_bot', 'Не криминал (не работа закладчиком и т.д.)')
+        client.send_message('@RuAnon_bot', 'Если интересует пиши сюда: @abbcddd . ')
         time.sleep(10)
     #client.send_message(""@RuAnon_bot", '/stop')
     #time.sleep(5)
