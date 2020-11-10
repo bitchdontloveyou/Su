4c26dbdb2c7dc99c89b30db9d638583e4a24998c
bitchdonotloveyou <64248540+bitchdontloveyou@users.noreply.github.com>
2020-11-10 12:18:29 +0300

    Update 8.py

--- 8.py
+++ 8.py
@@ -7,15 +7,19 @@ api_hash = '31e44198df1635fa81c45fd8a23ba8e0'
 #client = TelegramClient('session_name', api_id, api_hash)
 #client.start()
 with TelegramClient('name', api_id, api_hash) as client :
-    While True :
+    nixya = 5
+    while (nixya < 55) :
         client.send_message('RuAnon_bot', '/stop')
+        time.sleep(5)
         client.send_message('RuAnon_bot', '/start') 
+        time.sleep(10)
         client.send_message('RuAnon_bot', 'Работа интересует? Без 
 привязки к месту и возврасту. 
 Подойдет даже школьникам. Не 
 криминал (не работа закладчиком
 и т.д.) Если интересует пиши, 
 сюда: @abbcddd . ')
+        time.sleep(10)
     #client.send_message(""@RuAnon_bot", '/stop')
     #time.sleep(5)
     #client.send_message(""@RuAnon_bot", '/start' )
